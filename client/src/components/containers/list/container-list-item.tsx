033a1a6274240e3057855fb41957dab1d2967539
TheMineWay <joelcamposoliva9000@gmail.com>
2024-02-13 19:12:59 +0100

    Info display

--- client/src/components/containers/list/container-list-item.tsx
+++ client/src/components/containers/list/container-list-item.tsx
@@ -1,17 +1,35 @@
-import { Card, Flex, Typography } from "antd";
+import { Button, Card, Flex, Popover, Typography } from "antd";
 import { Container } from "../../../models/containers/container.model";
+import { useNavigate } from "react-router-dom";
+import { routes } from "../../../router/routes";
+import { InfoCircleOutlined } from "@ant-design/icons";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 type Props = {
   container: Container;
 };
 
-export default function ContainerListItem({ container }: Props) {
+export default function ContainerListItem({
+  container: { name, id, description },
+}: Props) {
+  const navigate = useNavigate();
+
   return (
-    <Card>
-      <Flex vertical gap={6}>
-        <Title level={3}>{container.name.trim()}</Title>
+    <Card hoverable onClick={() => navigate(routes.CONTAINER(id))}>
+      <Flex gap={6} justify="space-between">
+        <Title level={3}>{name.trim()}</Title>
+        {description && (
+          <Popover content={<Text>{description}</Text>}>
+            <Button
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              type="link"
+              icon={<InfoCircleOutlined />}
+            />
+          </Popover>
+        )}
       </Flex>
     </Card>
   );
--- client/src/components/containers/list/containers-list.tsx
+++ client/src/components/containers/list/containers-list.tsx
@@ -1,7 +1,7 @@
 import ManagedFetchRenderer from "react-data-fetch-manager/dist/components/renderers/managed-fetch-renderer";
 import { useContainers } from "../../../hooks/api/containers/use-containers";
 import ContainerListItem from "./container-list-item";
-import { Row } from "antd";
+import { Col, Row } from "antd";
 
 export default function ContainersList() {
   const managedFetch = useContainers();
@@ -10,9 +10,11 @@ export default function ContainersList() {
     <ManagedFetchRenderer
       managedFetch={managedFetch}
       render={({ rows }) => (
-        <Row>
+        <Row gutter={[12, 12]}>
           {rows.map((row) => (
-            <ContainerListItem container={row} key={row.id} />
+            <Col key={row.id} xs={24} md={12} xl={8} xxl={6}>
+              <ContainerListItem container={row} />
+            </Col>
           ))}
         </Row>
       )}
